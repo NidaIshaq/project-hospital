af096dd8e0e723ab630f4d4485a741ae4c840bb5
Nida Fatima <ishaqnida7@gmail.com>
2024-04-19 20:46:48 +0500

    yes

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -6,25 +6,18 @@ import Home from "./Pages/Home";
 import Appointment from "./Pages/Appointment";
 import AboutUs from "./Pages/AboutUs";
 import Register from "./Pages/Register";
-// import Footer from "./components/Footer";
-// import Navbar from "./components/Navbar";
 
-// import axios from "axios";
 import { Context } from "./main";
 import Login from "./Pages/Login";
-// import DashboardLayout from "../src/components/Layout/DashboardLayout";
-//import Dashboard from "./Dashboard/Dashboard";
+
 import DashboardLayout from "./components/Layout/DashboardLayout";
-// import Loginn from "./Dashboard/login";
-// import AddNewAdmin from "./Dashboard/AddNewAdmin";
-// import Messagess from "./Dashboard/Messages";
-// import Doctors from "./Dashboard/Doctors";
+
 
 function App() {
   return (
     <>
       <Router>
-        {/* <Navbar /> */}
+      
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/appointment" element={<Appointment />} />
@@ -32,13 +25,9 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/dashboardlayout" element={<DashboardLayout />} />
-          {/* <Route path="/login1" element={<Loginn />} />
-          <Route path="/doctor/addnew" element={<AddNewDoctor />} />
-          <Route path="/admin/addnew" element={<AddNewAdmin />} />
-          <Route path="/messages" element={<Messagess />} />
-          <Route path="/doctors" element={<Doctors />} /> */}
+          
         </Routes>
-        {/* <Footer /> */}
+       
       </Router>
     </>
   );
